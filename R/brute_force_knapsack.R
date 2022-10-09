522bdabae35e5864806405c3153cec43b80bdbce
Hussnain Ali <110018587+iamhussnainn@users.noreply.github.com>
2022-10-09 02:22:55 +0200

    dynamic knapsack fixes

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -45,3 +45,14 @@ brute_force_knapsack <-function(x, W){
     return (list("value"=finalValue, "output"=nodes))
   }
 
+
+RNGversion(min(as.character(getRversion()),"3.5.3"))
+set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+n <- 2000
+knapsack_objects <-
+  data.frame(
+    w=sample(1:4000, size = n, replace = TRUE),
+    v=runif(n = n, 0, 10000)
+  )
+
+knapsack_greedy(knapsack_objects[1:800,], 3500)
